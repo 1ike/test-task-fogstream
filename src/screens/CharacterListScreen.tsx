c69d9d7389131261d54d53c78f859ca171e52026
1ike <googlohren@gmail.com>
2022-07-10 22:04:02 +0300

    Add component CharacterCardLink.

--- src/screens/CharacterListScreen.tsx
+++ src/screens/CharacterListScreen.tsx
@@ -1,18 +1,12 @@
 import {
-  SafeAreaView, FlatList, View, StyleSheet, StatusBar, TouchableOpacity,
+  SafeAreaView, FlatList, StyleSheet, StatusBar,
 } from 'react-native';
-import { Card } from '@rneui/themed';
 import { ThunkDispatch } from 'redux-thunk';
 import { AnyAction } from 'redux';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { PropsWithChildren } from 'react';
-import { useNavigation } from '@react-navigation/native';
 
 import { RootState, useAppDispatch, useAppSelector } from '../state/store';
 import { selectCharacters, requestFetchCharacters } from '../state/characters';
-import Avatar from '../components/Avatar';
-import { Character } from '../types';
-import type { StackParamList } from '.';
+import CharacterCardLink from '../components/CharacterCardLink';
 
 
 const styles = StyleSheet.create({
@@ -20,69 +14,11 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
   },
-  link: {
-    alignSelf: 'stretch',
-    margin: 15,
-    marginBottom: 0,
-    flexGrow: 1,
-  },
-  cardContainerStyle: {
-    borderRadius: 5,
-  },
-  character: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  title: {
-    marginBottom: 0,
-    marginLeft: 10,
-  },
 });
 
 
-type NavProps = NativeStackNavigationProp<StackParamList, 'Character'>;
-
-interface Params {
-  character: Character,
-}
-interface To {
-  routeName: keyof StackParamList,
-  params: Params,
-}
-interface Props {
-  to: To,
-}
-
-
-function CardLink({ to, children }: PropsWithChildren<Props>) {
-  const { routeName, params: { character } } = to;
-  const navigation = useNavigation<NavProps>();
-
-  const onPress = () => {
-    navigation.navigate(routeName, { character });
-  };
-
-  return (
-    <TouchableOpacity onPress={onPress}>
-      {children}
-    </TouchableOpacity>
-  );
-}
-
-const renderItem = ({ item: character }: { item: Character }) => (
-  <CardLink to={{ routeName: 'Character', params: { character } }}>
-    <Card containerStyle={styles.cardContainerStyle}>
-      <View style={styles.character}>
-        <Avatar character={character} />
-        <Card.Title style={styles.title}>{character.name}</Card.Title>
-      </View>
-    </Card>
-  </CardLink>
-);
-
-
 export default function App() {
-  const result = useAppSelector(selectCharacters);
+  const characters = useAppSelector(selectCharacters);
 
   const dispatch = useAppDispatch();
 
@@ -95,8 +31,8 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
-        data={result}
-        renderItem={renderItem}
+        data={characters}
+        renderItem={CharacterCardLink}
         keyExtractor={(item) => String(item.id)}
         onEndReachedThreshold={0.7}
         onEndReached={onEndReached}
