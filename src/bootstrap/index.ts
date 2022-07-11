b4291aec5bc64fde43beb10ed8669dca9fe14dda
1ike <googlohren@gmail.com>
2022-07-11 15:57:11 +0300

    Fix persist.

--- src/bootstrap/index.ts
+++ src/bootstrap/index.ts
@@ -1,19 +1,13 @@
-// import 'dayjs/locale/ru';
-// import dayjs from 'dayjs';
-
 import store from '../state/store';
-// import persist from './persist';
+import persist from './persist';
 import fonts from './fonts';
 import fetchCharacters from './fetchCharacters';
 
 
 export const beforeRender = async () => {
-  // dayjs.locale('ru');
-  // await persist(store);
+  await persist(store);
   await fonts();
   await fetchCharacters(store);
 };
 
-export const afterRender = async () => {
-
-};
+export const afterRender = async () => {};
