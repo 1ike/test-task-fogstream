7ee59add2685f410a9b6febe928d4f6ebd51dd43
1ike <googlohren@gmail.com>
2022-07-11 18:34:50 +0300

    Add rest character details to Character screen.

--- src/screens/CharacterScreen.tsx
+++ src/screens/CharacterScreen.tsx
@@ -1,28 +1,69 @@
-import { View, StyleSheet, StatusBar } from 'react-native';
-import { Card } from '@rneui/themed';
+import {
+  View, StyleSheet, StyleProp, ViewStyle,
+} from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { get } from 'lodash';
+import { Text } from '@rneui/themed';
 
-import Avatar from '../components/character/Avatar';
+import CharacterCard from '../components/character/CharacterCard';
 import type { StackParamList } from '.';
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: StatusBar.currentHeight || 0,
+  cardHeader: {
+    marginBottom: 20,
   },
-  containerStyle: {
-    borderRadius: 5,
-  },
-  character: {
+  row: {
     flexDirection: 'row',
     alignItems: 'center',
   },
-  title: {
-    marginBottom: 0,
+  label: {
+    width: 70,
+    fontSize: undefined,
+  },
+  labelMargin: {
+    marginBottom: 10,
+  },
+  value: {
     marginLeft: 10,
   },
 });
+
+interface RowProps {
+  label: string, value: string, style?: StyleProp<ViewStyle>,
+}
+
+function Row({ label, value, style }: RowProps) {
+  return (
+    <View style={[styles.row, style && style]}>
+      <Text h4 h4Style={styles.label}>
+        {label}
+        :
+      </Text>
+      <Text style={styles.value}>{value}</Text>
+    </View>
+  );
+}
+
+interface DetailData {
+  label: string,
+  path: string,
+}
+const detailsData: DetailData[] = [
+  {
+    label: 'Gender',
+    path: 'gender',
+  },
+  {
+    label: 'Origin',
+    path: 'origin.name',
+  },
+  {
+    label: 'Location',
+    path: 'location.name',
+  },
+];
+
 
 type Props = NativeStackScreenProps<StackParamList, 'Character'>;
 
@@ -30,11 +71,15 @@ export default function App({ route }: Props) {
   const { character } = route.params;
 
   return (
-    <Card containerStyle={styles.containerStyle}>
-      <View style={styles.character}>
-        <Avatar character={character} />
-        <Card.Title style={styles.title}>{character.name}</Card.Title>
-      </View>
-    </Card>
+    <CharacterCard character={character} headerStyle={styles.cardHeader}>
+      {detailsData.map((detail, index, arr) => (
+        <Row
+          label={detail.label}
+          value={String(get(character, detail.path))}
+          key={detail.label}
+          style={index !== arr.length - 1 ? styles.labelMargin : null}
+        />
+      ))}
+    </CharacterCard>
   );
 }
